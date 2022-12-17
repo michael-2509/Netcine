7e6193c0665d2df1cd94c435d3a48dd2c4c0f5fb
Adigun Idowu Michael <idowumichael2509@gmail.com>
2022-12-17 10:20:43 +0100

    implemented tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <h1>Netcine</h1>
+    <div>
+      <h1 text-white px-4 py-2 bg-sky-700>
+        Netcine
+      </h1>
       <p>No movies to watch yet, Let's Build</p>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //mode is for "just in time" to complile your css on the file
+
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
