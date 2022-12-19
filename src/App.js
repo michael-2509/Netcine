c90222c9651338053ce8e5f2c13234cee95ae983
Adigun Idowu Michael <idowumichael2509@gmail.com>
2022-12-19 10:05:49 +0100

    added jsconfig file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1 text-white px-4 py-2 bg-sky-700>
-        Netcine
-      </h1>
+    <div className="m-auto block text-white">
+      <h1 className="text-5xl">Netcine</h1>
       <p>No movies to watch yet, Let's Build</p>
     </div>
   );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <section>{/* <div><img src={} /></div> */}</section>
+    </>
+  );
+};
+
+export default Nav;
